69ff918990a0efd0b0489417ec1dc5d84db6ce87
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-05-09 12:15:54 -0700

    Add User Input to CameraController.gd

--- CasaGrande/CameraController.gd
+++ CasaGrande/CameraController.gd
@@ -3,6 +3,9 @@ extends Camera3D
 @export
 var radius = 100
 
+@export
+var speed = 1000
+
 var x = 0
 var y = 0
 
@@ -10,12 +13,15 @@ var y = 0
 func _ready():
 	pass # Replace with function body.
 
-func get_user_input():
-	pass
+func get_user_input(delta):
+	if (Input.is_action_pressed("A")):
+		x += -1 * speed * delta
+	elif (Input.is_action_pressed("D")):
+		x += 1 * speed * delta
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_user_input()
+	get_user_input(delta)
 	
 	var pos = Vector3(radius * cos(x / (2 * PI * radius)), 5.771, 
 			radius * sin(x / (2 * PI * radius)))
