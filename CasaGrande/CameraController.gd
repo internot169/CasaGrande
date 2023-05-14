2143738d9369d0138551a38a6d4bc5602417dd49
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-05-14 15:10:57 -0700

    Added Board rotation, turn end, burn to UI.tscn

--- CasaGrande/CameraController.gd
+++ CasaGrande/CameraController.gd
@@ -1,33 +1,3 @@
-extends Camera3D
+extends Node3D
 
-@export
-var radius = 100
-
-@export
-var speed = 1000
-
-var x = 0
-var y = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func get_user_input(delta):
-	if (Input.is_action_pressed("A")):
-		x += -1 * speed * delta
-	elif (Input.is_action_pressed("D")):
-		x += 1 * speed * delta
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	get_user_input(delta)
-	
-	var pos = Vector3(radius * cos(x / (2 * PI * radius)), 5.771, 
-			radius * sin(x / (2 * PI * radius)))
-	
-	print(x)
-	print(y)
-	print(pos)
-	
-	self.position = pos
+var input
--- CasaGrande/ui.gd
+++ CasaGrande/ui.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var textbox_open = false
+var slider_value = 0
 
 func display_dice(num):
 	var dice = $Dice
@@ -16,3 +18,25 @@ func display_player(p: Player, text):
 	label.text = text
 	label.text += "\nCoins: " + str(p.money)
 	label.text += "\nTokens: " + str(p.tokens_left)
+
+
+func _on_button_pressed():
+	get_node("..").switch_turn()
+
+func _ready():
+	$Movemore/TextEdit.visible = false
+
+func _on_movemore_pressed():
+	if(textbox_open):
+		var amount = int($Movemore/TextEdit.text)
+		get_node("..").move_token(amount)
+		$Movemore/TextEdit.visible = false
+	else:
+		$Movemore/TextEdit.visible = true
+		$Movemore/TextEdit.text = ""
+	textbox_open = !textbox_open
+
+
+func _on_h_slider_value_changed(value):
+	get_node("/root/Main/CameraHolder").input = value
+	print(get_node("/root/Main/CameraHolder").input)
