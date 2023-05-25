dd9cc15e6d6074f8055fb9a893d8a40d96cf985b
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-05-25 11:02:28 -0700

    Render highlighter to mark where Inputting box

--- CasaGrande/input_getter.gd
+++ CasaGrande/input_getter.gd
@@ -7,7 +7,7 @@ var y = 0
 func _ready():
 	pass # Replace with function body.
 
-func _unhandled_input(event):
+func get_input():
 	if Input.is_action_pressed("D"):
 		x += 1
 	elif Input.is_action_pressed("A"):
@@ -19,7 +19,9 @@ func _unhandled_input(event):
 	
 
 func _process(_delta):
-	var bounds = get_node("..").get_node("../GameManager").get_x_y_bound()
+	get_input()
+	
+	var bounds = get_node("../GameManager").get_x_y_bound()
 	var x_left_bound = bounds[0]
 	var x_right_bound = bounds[1]
 	var y_left_bound = bounds[2]
@@ -32,11 +34,20 @@ func _process(_delta):
 		x = ((x - x_left_bound) % (x_right_bound - x_left_bound)) + x_left_bound
 	elif check_y:
 		y = ((y - y_left_bound) % (y_right_bound - y_left_bound)) + y_left_bound
-	
+	else:
+		x %= 20
+		y %= 20
 	display_input_box()
 		
 func display_input_box():
-	pass
+	# Each box is width five and height 5
+	# Means position of display starts at 2.5 and increases by 5
+	# x starts at 0, z starts at -88
+	var z_amt = -88
+	var x_amt = 0
+	var pos:Vector3 = Vector3(x_amt + (2.5 * x), 0.6, z_amt + (2.5 * y))
+	
+	get_node("Highlighterinput").position = pos
 
 func reset():
 	x = 0
